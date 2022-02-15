a1bf0569d62728e479b775bd11d6f21c31041f51
Ara-Chobanyan <arachobanyan2001@gmail.com>
2022-02-15 14:18:31 -0600

    Added better tests for stringtoslice

--- cmd/stringtoslice/stringtoslice_test.go
+++ cmd/stringtoslice/stringtoslice_test.go
@@ -5,12 +5,27 @@ import (
 )
 
 func TestStringToSlice(t *testing.T) {
-	strings := "55"
-
-	got := ConvertStringToSlice(strings)
-	want := []int{55}
-
-	if got[0] != want[0] {
-		t.Errorf("got %d want %d given, %v", got, want, strings)
+	testCases := []struct {
+		input  string
+		output []int
+	}{
+		{"55", []int{55}},
+		{"100", []int{100}},
+		{"77", []int{77}},
+		{"88", []int{88}},
+		{"11", []int{11}},
+		{"45", []int{45}},
+		{"89", []int{89}},
+		{"0", []int{0}},
 	}
+
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			actual := ConvertStringToSlice(tc.input)
+			if actual[0] != tc.output[0] {
+				t.Errorf("got %d; want %d", tc.output, actual)
+			}
+		})
+	}
+
 }
