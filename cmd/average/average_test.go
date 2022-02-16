2cabe3ca967be7c83b59971a9a2348dac1944aa4
Ara-Chobanyan <arachobanyan2001@gmail.com>
2022-02-15 20:24:33 -0600

    Improved on average test file

--- cmd/average/average_test.go
+++ cmd/average/average_test.go
@@ -3,12 +3,26 @@ package average
 import "testing"
 
 func TestFindAverage(t *testing.T) {
-	average := FindAverage([]int{33, 44, 55, 66})
-	expected := float32(49.5)
-
-	if average != expected {
-		t.Errorf("expected %f but got %f", expected, average)
+	testCases := []struct {
+		input []int
+		want  float32
+	}{
+		{[]int{33, 44, 55, 66}, 49.5},
+		{[]int{50, 100, 87, 66}, 75.75},
+		{[]int{100, 100, 100, 100}, 100.0},
+		{[]int{98, 77, 33, 78}, 71.5},
+		{[]int{89, 51, 45, 100}, 71.25},
 	}
+
+	for _, tc := range testCases {
+		t.Run("check average", func(t *testing.T) {
+			actual := FindAverage(tc.input)
+			if actual != tc.want {
+				t.Errorf("got %f; want %f", actual, tc.want)
+			}
+		})
+	}
+
 }
 
 func BenchmarkFindAverage(b *testing.B) {
